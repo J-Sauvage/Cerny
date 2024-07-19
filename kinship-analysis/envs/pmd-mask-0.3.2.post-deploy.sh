7b17c3ee69ab3bd0d897622b0ef2a7a47dcdb884
Mael Lefeuvre <lefeuvre.m.p@gmail.com>
2024-07-19 15:04:16 +0200

    Anonymize Github URL (flag culprit lines with '#[PLACEHOLDER]')

--- kinship-analysis/envs/pmd-mask-0.3.2.post-deploy.sh
+++ kinship-analysis/envs/pmd-mask-0.3.2.post-deploy.sh
@@ -3,11 +3,7 @@
 eval "$(conda shell.bash hook)"
 conda activate "$(basename "${BASH_SOURCE[0]%.post-deploy.sh}")" 
 
-#RELEASE="https://github.com/MaelLefeuvre/pmd-mask/releases/download/v0.3.2/pmd-mask-0.3.2_x86_64-unknown-linux-gnu"
-#wget -O "${CONDA_PREFIX}/bin/pmd-mask" $RELEASE
-
-#REPO="https://github.com/MaelLefeuvre/pmd-mask.git"
-REPO="git@github.com:MaelLefeuvre/pmd-mask.git"
+REPO="https://github.com/xxxxxxxxx/pmd-mask.git" #[PLACEHOLDER]
 BUILD_DIR="$CONDA_PREFIX/$(basename "${REPO%.git}")"
 
 cd $CONDA_PREFIX
